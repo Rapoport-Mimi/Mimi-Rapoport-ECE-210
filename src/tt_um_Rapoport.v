a9e5e17a42e8a975ad774034c5817070c857471c
Rapoport-Mimi <mirapopo@ucsc.edu>
2024-11-09 21:58:35 -0500

    assigning uo_out to 0

--- src/tt_um_Rapoport.v
+++ src/tt_um_Rapoport.v
@@ -19,6 +19,7 @@ module tt_um_Rapoport (
   // All output pins must be assigned. If not used, assign to 0.
   assign uio_out = 0;
   assign uio_oe  = 0;
+  assign uo_out = 0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
