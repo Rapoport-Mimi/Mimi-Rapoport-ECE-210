463058a306e067a67976e74b70c3dfe7097951a0
Rapoport-Mimi <mirapopo@ucsc.edu>
2024-11-09 19:33:25 -0500

    update makerfile project sources

--- src/tt_um_Rapoport.v
+++ src/tt_um_Rapoport.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module parent (
+module tt_um_Rapoport (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
