1da08fb942543b89e893b72d79be5b845e1738b6
Rapoport-Mimi <mirapopo@ucsc.edu>
2024-11-09 21:52:24 -0500

    fixed silly error

--- src/functions.v
+++ src/functions.v
@@ -30,8 +30,8 @@ module perceptron (
 
             if (out != desired_out) begin
                 we1 <= we1 + ((({15'b0, desired_out} - {15'b0, out}) * in1) / LEARNING_RATE_MULT_INV);
-                we2 <= we2 + ((({15'b0, desired_out} - {15'b0, out}) / LEARNING_RATE_MULT_INV); 
-                we3 <= we3 + ((({15'b0, desired_out} - {15'b0, out}) / LEARNING_RATE_MULT_INV);
+                we2 <= we2 + ((({15'b0, desired_out} - {15'b0, out}) * in2) / LEARNING_RATE_MULT_INV); 
+                we3 <= we3 + ((({15'b0, desired_out} - {15'b0, out}) * in3) / LEARNING_RATE_MULT_INV);
             end
         end
     end
