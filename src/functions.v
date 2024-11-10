78f225bc65791d93a615b18a9bd9d156807dc016
Rapoport-Mimi <mirapopo@ucsc.edu>
2024-11-09 20:54:51 -0500

    potentially bad idea of using multiplicative inverses; changed input bits

--- src/functions.v
+++ src/functions.v
@@ -3,40 +3,40 @@
 module perceptron (
     input wire          clk,
     input wire          reset,
-    input wire  [7:0]   in1,
-    input wire  [7:0]   in2,
-    input wire  [7:0]   in3,
+    input wire  [3:0]   in1,
+    input wire  [3:0]   in2,
+    input wire  [6:0]   in3,
     output reg  [0:0]   out,
-    output reg  [0:0]   desired_out
+    input wire  [0:0]    desired_out
 );
 
     reg    [15:0]   threshold;
-    reg    [15:0]   we1;
-    reg    [15:0]   we2;
-    reg    [15:0]   we3;
+    reg    [3:0]   we1_mult_inv;
+    reg    [3:0]   we2_mult_inv;
+    reg    [6:0]   we3_mult_inv;
     wire   [15:0]   weighted;
-    parameter ONE_OVER_LEARNING_RATE = 8'd10;
+    parameter LEARNING_RATE_MULT_INV = 16'd10;
 
     always @(posedge clk or negedge reset) begin
         if (!reset) begin
             out <= 0;
-            threshold <= 200; 
-            we1 <= 16'd10;       
-            we2 <= 16'd20;       
-            we3 <= 16'd30; 
+            threshold <= 16'd200; 
+            we1_mult_inv <= 4'd10;       
+            we2_mult_inv <= 4'd20;       
+            we3_mult_inv <= 6'd30; 
         end
         else begin
             out <= (weighted >= threshold);
 
             if (out != desired_out) begin
-                we1 <= we1 + ((desired_out - out) * in1) / ONE_OVER_LEARNING_RATE;
-                we2 <= we2 + ((desired_out - out) * in2) / ONE_OVER_LEARNING_RATE; 
-                we3 <= we3 + ((desired_out - out) * in3) / ONE_OVER_LEARNING_RATE;
+                we1_mult_inv <= 1 / ( (1 / we1_mult_inv) + (desired_out - out) * in1 / LEARNING_RATE_MULT_INV );
+                we2_mult_inv <= 1 / ( (1 / we2_mult_inv) + (desired_out - out) * in2 / LEARNING_RATE_MULT_INV ); 
+                we3_mult_inv <= 1 / ( (1 / we3_mult_inv) + (desired_out - out) * in3 / LEARNING_RATE_MULT_INV );
             end
         end
     end
 
     // summation logic
-    assign weighted = in1 * we1 + in2 * we2 + in3 * we3; 
+    assign weighted = in1 / we1_mult_inv + in2 / we2_mult_inv + in3 / we3_mult_inv; 
 
 endmodule
--- src/tt_um_Rapoport.v
+++ src/tt_um_Rapoport.v
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Mimi Rapoport
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -17,11 +17,21 @@ module tt_um_Rapoport (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
+
+  // Instantiate perceptron
+  perceptron perceptron1 (
+    .clk(clk), 
+    .reset(rst_n), 
+    .in1(ui_in[3:0]), 
+    .in2(ui_in[7:4]), 
+    .in3(uio_in[6:0]), 
+    .desired_out(ui_in[7]), 
+    .out(uio_out[7])
+  );
 
 endmodule
