05bee264fded112667ca1d0c6dfc9c7d4407472a
Rapoport-Mimi <mirapopo@ucsc.edu>
2024-11-21 18:32:58 -0500

    Update bits in functions.v

--- src/functions.v
+++ src/functions.v
@@ -5,25 +5,25 @@ module perceptron (
     input wire          reset,
     input wire  [3:0]   in1,
     input wire  [3:0]   in2,
-    input wire  [6:0]   in3,
+    input wire  [3:0]   in3,
     output reg  [0:0]   out,
     input wire  [0:0]    desired_out
 );
 
-    reg    [15:0]   threshold;
-    reg    [15:0]   we1;
-    reg    [15:0]   we2;
-    reg    [15:0]   we3;
-    wire   [15:0]   weighted;
+    reg    [3:0]   threshold;
+    reg    [3:0]   we1;
+    reg    [3:0]   we2;
+    reg    [3:0]   we3;
+    wire   [3:0]   weighted;
     parameter LEARNING_RATE_MULT_INV = 16'd10;
 
     always @(posedge clk or negedge reset) begin
         if (!reset) begin
             out <= 0;
-            threshold <= 16'd200; 
-            we1 <= 16'd10;       
-            we2 <= 16'd20;       
-            we3 <= 16'd30; 
+            threshold <= 4'd15; 
+            we1 <= 4'd5;       
+            we2 <= 4'd4;       
+            we3 <= 4'd3; 
         end
         else begin
 
@@ -31,9 +31,9 @@ module perceptron (
             
             // Weight updates
             if (out != desired_out) begin
-                we1 <= we1 + ((({15'b0, desired_out} - {15'b0, out}) * in1) / LEARNING_RATE_MULT_INV);
-                we2 <= we2 + ((({15'b0, desired_out} - {15'b0, out}) * in2) / LEARNING_RATE_MULT_INV); 
-                we3 <= we3 + ((({15'b0, desired_out} - {15'b0, out}) * in3) / LEARNING_RATE_MULT_INV);
+                we1 <= we1 + (({4'b0, (desired_out - out)} * in1) / LEARNING_RATE_MULT_INV);
+                we2 <= we2 + (({4'b0, (desired_out -  out)} * in2) / LEARNING_RATE_MULT_INV); 
+                we3 <= we3 + (({4'b0, (desired_out -  out)} * in3) / LEARNING_RATE_MULT_INV);
             end
         end
     end
